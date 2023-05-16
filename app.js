4b7a28ffa3c783b2c163883a9bce64e5202ba68c
PH-68 <58099156+PH-68@users.noreply.github.com>
2023-05-16 20:45:15 +0800

    Update detection rule

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const mineflayer = require('mineflayer')
 require('dotenv').config();
 const bot = mineflayer.createBot({
-    host: 'mcfallout.net', // minecraft server ip
-    username: 'ianhong68@outlook.com', // minecraft username
+    host: 'jp.mcfallout.net', // minecraft server ip
+    username: process.env.email, // minecraft username
     auth: 'microsoft', // for offline mode servers, you can set this to 'offline'
     // port: 25565,                // only set if you need a port that isn't 25565
     version: "1.19.3",             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
     // password: '12345678'        // set if you want to use password-based auth (may be unreliable)
 })
 
-var rate = "", flag = false
+var rate = "", lock = false
 
 
 bot.on('spawn', (username, message) => {
@@ -24,9 +24,9 @@ bot.on('spawn', (username, message) => {
 bot.on('message', async (jsonMsg, position, sender, verified) => {
     //if (username === bot.username) return
     //bot.chat(message)
-    if (flag) {
+    if (!lock && jsonMsg.toString().indexOf(new Date(new Date().setHours(new Date().getHours() + 8)).toJSON().slice(0, 10)) == 0) {
+        lock = true
         rate = jsonMsg.toString()
-        flag = false
         console.warn(rate)
         var response = await (await fetch("https://api.github.com/repos/PH-68/FalloutExchangeRate/contents/rate.csv?ref=data", { method: "GET", headers: { Authorization: `Bearer ${process.env.gh_pat}` } })).json();
         if (Buffer.from(response.content, 'base64').toString().indexOf(rate.split(",")[0]) != -1) {
@@ -35,11 +35,9 @@ bot.on('message', async (jsonMsg, position, sender, verified) => {
         }
         var content = `{\"message\":\"automated comment\",\"content\":\"${Buffer.from(Buffer.from(response.content, 'base64').toString() + rate + "\n").toString("base64")}\",\"sha\":\"${response.sha}\",\"branch\":\"data\"}`
         response = await fetch("https://api.github.com/repos/PH-68/FalloutExchangeRate/contents/rate.csv?ref=data", { method: "PUT", body: content, headers: { Authorization: `Bearer ${process.env.gh_pat}` } });
+        lock = false
         console.log(await response.status)
         process.exit(await response.status)
-    }
-    if (jsonMsg.toString().includes("cepool-data-api-price")) {
-        flag = true
     }
     //console.log(jsonMsg.toString())
 })
