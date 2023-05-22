b31fc5b5cbd960b3b623fa721686193b7afa0245
PH-68 <58099156+PH-68@users.noreply.github.com>
2023-05-22 19:08:04 +0800

    Support SRI

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import path from 'path';
+import { defineConfig } from 'vite'
+import sri from '@small-tech/vite-plugin-sri'
 
-export default {
+export default defineConfig({
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   },
   server: {
-    port: 8080,
-    hot: true
+    port: 8080
   },
-  base: "./"
-}
+  plugins: [sri()]
+})
